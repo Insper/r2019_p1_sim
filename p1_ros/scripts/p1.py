76dca99236ebcd5ef0d98427448bc766ff8ddd7b
mirwox <fmiranda@gmail.com>
2019-04-02 14:17:26 -0300

    p1 - só falta acertar o hough circles

--- p1_ros/scripts/p1.py
+++ p1_ros/scripts/p1.py
@@ -33,90 +33,140 @@ area = 0.0 # Variavel com a area do maior contorno
 check_delay = False 
 
 
+def auto_canny(image, sigma=0.33):
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+
+    # return the edged image
+    return edged
+
+def find_circles(imagem_bgr):
+
+    gray = cv2.cvtColor(imagem_bgr, cv2.COLOR_BGR2GRAY)
+    # A gaussian blur to get rid of the noise in the image
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    #blur = gray
+    # Detect the edges present in the image
+    bordas = auto_canny(blur)
+    bordas_color = cv2.cvtColor(bordas, cv2.COLOR_GRAY2BGR)
+    circles = None
+
+    # Not: precisamos aumentar o maxRadius - veja a documentacao
+    circles=cv2.HoughCircles(bordas,cv2.HOUGH_GRADIENT,2,40,param1=50,param2=100,minRadius=5,maxRadius=500)
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+
+        for i in circles[0,:]:
+            print(i)
+            # draw the outer circle
+            # cv2.circle(img, center, radius, color[, thickness[, lineType[, shift]]])
+            cv2.circle(bordas_color,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(bordas_color,(i[0],i[1]),2,(0,0,255),3)
+
+    cv2.imshow("circulos", bordas_color)
+
+    if circles is not None:
+        return True
+    else:
+        return False
+
+
 # A função a seguir é chamada sempre que chega um novo frame
 def roda_todo_frame(imagem):
-	print("frame")
-	global cv_image
-	global media
-	global centro
+    print("frame")
+    global cv_image
+    global media
+    global centro
 
-	global viu_bird
+    global viu_bird
 
-	now = rospy.get_rostime()
-	imgtime = imagem.header.stamp
-	lag = now-imgtime # calcula o lag
-	delay = lag.nsecs
-	print("delay ", "{:.3f}".format(delay/1.0E9))
-	if delay > atraso and check_delay==True:
-		print("Descartando por causa do delay do frame:", delay)
-		return 
-	try:
-		antes = time.clock()
-		cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
-		centro, imagem, resultados =  visao_module.processa(cv_image)
+    now = rospy.get_rostime()
+    imgtime = imagem.header.stamp
+    lag = now-imgtime # calcula o lag
+    delay = lag.nsecs
+    print("delay ", "{:.3f}".format(delay/1.0E9))
+    if delay > atraso and check_delay==True:
+        print("Descartando por causa do delay do frame:", delay)
+        return 
+    try:
+        antes = time.clock()
+        cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
+        centro, imagem, resultados =  visao_module.processa(cv_image)
+        vc_temp = find_circles(cv_image)
+        if vc_temp:
+            viu_circulo = True
 
-		for r in resultados:
-			# print(r) - print feito para documentar e entender
-			# o resultado
-			if r[0] == "bird":
-				viu_bird = True
 
-		depois = time.clock()
-		cv2.imshow("Camera", cv_image)
-	except CvBridgeError as e:
-		print('ex', e)
-	
+        for r in resultados:
+            # print(r) - print feito para documentar e entender
+            # o resultado
+            if r[0] == "bird":
+                viu_bird = True
+
+        depois = time.clock()
+        cv2.imshow("Camera", cv_image)
+    except CvBridgeError as e:
+        print('ex', e)
+    
 if __name__=="__main__":
-	rospy.init_node("cor")
+    rospy.init_node("cor")
 
-	topico_imagem = "/kamera"
-	
-	# Para renomear a *webcam*
-	#   Primeiro instale o suporte https://github.com/Insper/robot19/blob/master/guides/debugar_sem_robo_opencv_melodic.md
-	#
-	#	Depois faça:
-	#	
-	#	rosrun cv_camera cv_camera_node
-	#
-	# 	rosrun topic_tools relay  /cv_camera/image_raw/compressed /kamera
-	#
-	# 
-	# Para renomear a câmera simulada do Gazebo
-	# 
-	# 	rosrun topic_tools relay  /camera/rgb/image_raw/compressed /kamera
-	# 
-	# Para renomear a câmera da Raspberry
-	# 
-	# 	rosrun topic_tools relay /raspicam_node/image/compressed /kamera
-	# 
+    topico_imagem = "/kamera"
+    
+    # Para renomear a *webcam*
+    #   Primeiro instale o suporte https://github.com/Insper/robot19/blob/master/guides/debugar_sem_robo_opencv_melodic.md
+    #
+    #	Depois faça:
+    #	
+    #	rosrun cv_camera cv_camera_node
+    #
+    # 	rosrun topic_tools relay  /cv_camera/image_raw/compressed /kamera
+    #
+    # 
+    # Para renomear a câmera simulada do Gazebo
+    # 
+    # 	rosrun topic_tools relay  /camera/rgb/image_raw/compressed /kamera
+    # 
+    # Para renomear a câmera da Raspberry
+    # 
+    # 	rosrun topic_tools relay /raspicam_node/image/compressed /kamera
+    # 
 
-	recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
-	print("Usando ", topico_imagem)
+    recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
+    print("Usando ", topico_imagem)
 
-	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+    velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
-	try:
+    try:
 
-		while not rospy.is_shutdown():
-			vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
+        while not rospy.is_shutdown():
+            vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
 
-			if viu_bird:
-				vel = Twist(Vector3(0.4,0,0), Vector3(0,0,0))
-				velocidade_saida.publish(vel)
-				rospy.sleep(0.8)
-				viu_bird = False
-				continue
+            if viu_bird:
+                vel = Twist(Vector3(0.4,0,0), Vector3(0,0,0))
+                velocidade_saida.publish(vel)
+                rospy.sleep(0.8)
+                viu_bird = False
+                continue
 
-			if viu_circulo:
-				vel = Twist(Vector3(0.4,0,0), Vector3(0,0,0))
-				velocidade_saida.publish(vel)
-				rospy.sleep(0.8)
-				continue
+            if viu_circulo:
+                vel = Twist(Vector3(-0.4,0,0), Vector3(0,0,0))
+                velocidade_saida.publish(vel)
+                rospy.sleep(0.8)
+                viu_circulo = False
+                continue
 
-			velocidade_saida.publish(vel)
-			rospy.sleep(0.1)
+            velocidade_saida.publish(vel)
+            rospy.sleep(0.1)
 
-	except rospy.ROSInterruptException:
-	    print("Ocorreu uma exceção com o rospy")
+    except rospy.ROSInterruptException:
+        print("Ocorreu uma exceção com o rospy")
 
 
